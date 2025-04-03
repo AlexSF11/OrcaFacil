5b3fbbd160f1ed1212927c763d054f4b2c3b6002
AlexSF11 <alexfreire836@gmail.com>
2025-04-03 13:12:19 -0300

    Ajuste no Layout do MyBudgets

--- app/src/main/java/com/example/orcafacil/BudgetAdapter.kt
+++ app/src/main/java/com/example/orcafacil/BudgetAdapter.kt
@@ -1,29 +1,43 @@
 package com.example.orcafacil
 
 import android.content.Intent
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orcafacil.model.Budget
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class BudgetAdapter(private val budgets: List<Budget>) :
     RecyclerView.Adapter<BudgetAdapter.BudgetViewHolder>() {
 
     class BudgetViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvBudgetName: TextView = itemView.findViewById(R.id.tvBudgetName)
+        val tvBudgetCreatedDate: TextView = itemView.findViewById(R.id.tvBudgetCreatedDate)
+        val tvBudgetTotalPrice: TextView = itemView.findViewById(R.id.tvBudgetTotalPrice)
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BudgetViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_budget, parent, false)
         return BudgetViewHolder(view)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: BudgetViewHolder, position: Int) {
         val budget = budgets[position]
         holder.tvBudgetName.text = budget.name
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        holder.tvBudgetCreatedDate.text = dateFormat.format(budget.createdDate)
+        val currencyFormat = NumberFormat.getCurrencyInstance(Locale("pt", "BR"))
+        holder.tvBudgetTotalPrice.text = currencyFormat.format(budget.totalPrice)
+
 
         // Adicionar listener de clique para abrir a tela de edição
         holder.itemView.setOnClickListener {
--- app/src/main/java/com/example/orcafacil/MyBudgets.kt
+++ app/src/main/java/com/example/orcafacil/MyBudgets.kt
@@ -23,7 +23,6 @@ class MyBudgets : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_my_budgets)
 
         // Inicializar o RecyclerView e o TextView
